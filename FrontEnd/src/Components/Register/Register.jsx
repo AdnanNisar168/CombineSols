f4f795b83a84bc4f254a5acd60a0a1fcce59efa8
Faisal Saleem <fysalsaleem@gmail.com>
2025-01-11 22:56:41 +0500

    Register

--- FrontEnd/src/Components/Register/Register.jsx
+++ FrontEnd/src/Components/Register/Register.jsx
@@ -33,61 +33,66 @@ const Register = () => {
         onSubmit={handleSubmit}
       >
         {() => (
-          <Form>
-            <div>
-              <label htmlFor="username">User Name:</label>
-              <Field
-                type="text"
-                id="username"
-                name="username"
-                placeholder="Enter username"
-              />
-              <ErrorMessage name="username" component="div" className="error" />
-            </div>
-            <div>
-              <label htmlFor="firstname">First Name:</label>
-              <Field
-                type="text"
-                id="firstname"
-                name="firstname"
-                placeholder="Enter firstname"
-              />
-              <ErrorMessage name="firstname" component="div" className="error" />
-            </div>
-            <div>
-              <label htmlFor="lastname">Last Name:</label>
-              <Field
-                type="text"
-                id="lastname"
-                name="lastname"
-                placeholder="Enter lastname"
-              />
-              <ErrorMessage name="lastname" component="div" className="error" />
-            </div>
-  
-            <div>
-              <label htmlFor="password">Password:</label>
-              <Field
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Enter password"
-              />
-              <ErrorMessage name="password" component="div" className="error" />
-            </div>
-            <div>
-              <label htmlFor="confirmpassword">Confirm Password:</label>
-              <Field
-                type="password"
-                id="confirmpassword"
-                name="confirmpassword"
-                placeholder="Enter Confirm password"
-              />
-              <ErrorMessage name="password" component="div" className="error" />
-            </div>
-  
-            <button type="submit">Submit</button>
-          </Form>
+          <div className="flex items-center content-center min-h-100 h-screen bg-[#4b4b4b]">
+            <Form >
+              <div className="flex flex-col items-center justify-center h-screen">
+                <div>
+                  <label htmlFor="username">User Name:</label>
+                  <Field
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Enter username"
+                    className="w-full max-w-md bg-[#222] rounded-xl shadow-md py-8 px-8"
+                  />
+                  <ErrorMessage name="username" component="div" className="error" />
+                </div>
+                <div>
+                  <label htmlFor="firstname">First Name:</label>
+                  <Field
+                    type="text"
+                    id="firstname"
+                    name="firstname"
+                    placeholder="Enter firstname"
+                  />
+                  <ErrorMessage name="firstname" component="div" className="error" />
+                </div>
+                <div>
+                  <label htmlFor="lastname">Last Name:</label>
+                  <Field
+                    type="text"
+                    id="lastname"
+                    name="lastname"
+                    placeholder="Enter lastname"
+                  />
+                  <ErrorMessage name="lastname" component="div" className="error" />
+                </div>
+      
+                <div>
+                  <label htmlFor="password">Password:</label>
+                  <Field
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Enter password"
+                  />
+                  <ErrorMessage name="password" component="div" className="error" />
+                </div>
+                <div>
+                  <label htmlFor="confirmpassword">Confirm Password:</label>
+                  <Field
+                    type="password"
+                    id="confirmpassword"
+                    name="confirmpassword"
+                    placeholder="Enter Confirm password"
+                  />
+                  <ErrorMessage name="password" component="div" className="error" />
+                </div>
+      
+                <button type="submit">Submit</button>
+              </div>
+            </Form>
+          </div>
         )}
       </Formik>
     );
